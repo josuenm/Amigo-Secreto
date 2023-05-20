50f9930e7ad370407f03c65a5b938df0c7e9a1b5
josuenm <direct.josue@gmail.com>
2023-05-20 13:05:22 -0300

    feat: metodo para página de criar usuário

--- app/Http/Controllers/Auth/HandlePersonController.php
+++ app/Http/Controllers/Auth/HandlePersonController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\View\Components\HandlePerson;
 use Illuminate\Http\Request;
 
 class HandlePersonController extends Controller
 {
     public function createPersonPage()
     {
-        return view('auth.create-person');
+        $route = route('auth.create-person');
+        $title = 'Criar Pessoa';
+        $headline = 'Crie uma pessoa';
+        $description = 'Crie um usuário para fazer o amigo secreto, coloque obrigatóriamente o nome e email';
+
+        return view('auth.handle-person', compact('route', 'title', 'headline', 'description'));
     }
 
     public function editPersonPage()
+    {
+    }
+
+    public function createPerson()
     {
     }
 }
