23323e8e76cfcbd82b51eba7d512fa4c6ed156b9
josuenm <direct.josue@gmail.com>
2023-05-20 22:19:54 -0300

    feat: função para verificar rotas

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,11 +3,28 @@
     <link rel="stylesheet" href="{{ asset('css/components/header.css') }}">
 @endpush
 
+<?php
+
+    function urlWithNoBackButton() {
+        $urls = ['/', '/login', '/register'];
+        return in_array($_SERVER['REQUEST_URI'], $urls);
+    }
+
+?>
+
 <header class="header">
-    <div class="safe-area d-flex @if($_SERVER['REQUEST_URI'] !== '/') justify-content-between @else justify-content-center @endif">
-        @if($_SERVER['REQUEST_URI'] !== '/')
+    <div class="safe-area d-flex
+            @if(!urlWithNoBackButton())
+                justify-content-between
+            @else
+                justify-content-center
+            @endif
+        ">
+
+        @if(!urlWithNoBackButton())
             <button class="header_back-button" onclick="back()">Voltar</button>
         @endif
+
         <strong>
             Amigo Secreto
         </strong>
