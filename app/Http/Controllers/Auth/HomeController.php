6de2efae640a44ab5768a787aaa4f5bce7b516ce
josuenm <direct.josue@gmail.com>
2023-05-21 12:15:34 -0300

    feat: retirando métodos de verificação de autenticação

--- app/Http/Controllers/Auth/HomeController.php
+++ app/Http/Controllers/Auth/HomeController.php
@@ -10,10 +10,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        if (!Auth::check()) {
-            return redirect()->route('login.page');
-        }
-
         $people = [
             [
                 'name' => 'João',
