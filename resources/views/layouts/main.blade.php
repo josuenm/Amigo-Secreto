660c591b674b68af7a8f4c96e7f340e41984b98f
josuenm <direct.josue@gmail.com>
2023-05-20 22:21:53 -0300

    feat: novo titulo da página

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     @stack('styles')
 
-    <title>@yield('title', '|Amigo Secreto')</title>
+    <title>@yield('title') | Amigo Secreto</title>
 </head>
 
 <body>
