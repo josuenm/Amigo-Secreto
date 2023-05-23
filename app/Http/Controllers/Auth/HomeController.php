0ab26f1438e1216304246ded73e817919afe682d
josuenm <direct.josue@gmail.com>
2023-05-22 23:58:16 -0300

    feat: pegando os mais recentes

--- app/Http/Controllers/Auth/HomeController.php
+++ app/Http/Controllers/Auth/HomeController.php
@@ -12,14 +12,14 @@ class HomeController extends Controller
     public function index()
     {
         $givewaysEmpty = empty(Giveway::where('user_id', auth()->user()->id)->get()->first());
-        $people = Person::where('user_id', auth()->user()->id)->get()->toArray();
+        $people = Person::latest('updated_at')->where('user_id', auth()->user()->id)->get()->toArray();
 
         return view('auth.home', compact('people', 'givewaysEmpty'));
     }
 
     public function searchPeople(Request $req)
     {
-        $people = Person::where('user_id', auth()->user()->id)
+        $people = Person::latest('updated_at')->where('user_id', auth()->user()->id)
             ->where('name', 'LIKE', '%' . $req->search . '%')
             ->orWhere('email', 'LIKE', '%' . $req->search . '%')
             ->get()->toArray();
